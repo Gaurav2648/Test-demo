1502dab6d5019b4e8a27d404838160f1ff0cef3f
Gaurav2648 <gp112404@gmail.com>
2024-07-30 09:59:48 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	i am giving self error to see trigger error in jenkins
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
